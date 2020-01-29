73aa8fc22a089b24ba37cebfb4367150f72f2d70
Spencer Carli <spencercarli@gmail.com>
2020-01-29 09:31:54 -0600

    Rename mock component

--- test-config/mockComponent.js
+++ test-config/mockComponent.js
@@ -1,14 +1,13 @@
 export const mockComponent = (moduleName, propOverrideFn = () => {}) => {
   const RealComponent = require.requireActual(moduleName);
   const React = require("react");
-  const MockTouchableOpacity = props => {
+  const CustomizedComponent = props => {
     return React.createElement(
-      "TouchableOpacity",
-      // { ...props, onPress: disabled ? () => {} : onPress },
+      "CustomizedComponent",
       { ...props, ...propOverrideFn(props) },
       props.children
     );
   };
-  MockTouchableOpacity.propTypes = RealComponent.propTypes;
-  return MockTouchableOpacity;
+  CustomizedComponent.propTypes = RealComponent.propTypes;
+  return CustomizedComponent;
 };
