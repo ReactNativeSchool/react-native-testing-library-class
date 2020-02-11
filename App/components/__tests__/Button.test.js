49cfccdb7b189b365d7dc0eaa6af4bc8b5063d98
Spencer Carli <spencercarli@gmail.com>
2020-02-11 10:23:55 -0600

    09 Mocking Library

--- App/components/__tests__/Button.test.js
+++ App/components/__tests__/Button.test.js
@@ -1,7 +1,23 @@
 import React from "react";
 import { render, fireEvent } from "react-native-testing-library";
 
+import { mockComponent } from "../../../testing-config/mockComponent";
+
 import { Button } from "../Button";
+
+jest.mock(
+  "react-native/Libraries/Components/Touchable/TouchableOpacity",
+  () => {
+    return mockComponent(
+      "react-native/Libraries/Components/Touchable/TouchableOpacity",
+      props => {
+        return {
+          onPress: props.disabled ? () => {} : props.onPress
+        };
+      }
+    );
+  }
+);
 
 test("can press the button", () => {
   const onPressMock = jest.fn();
@@ -14,4 +30,14 @@ test("can press the button", () => {
 
   fireEvent.press(getByText("Testing"));
   expect(onPressMock.mock.calls.length).toBe(2);
+});
+
+test("onPress is not called when button is disabled", () => {
+  const onPressMock = jest.fn();
+  const { getByText } = render(
+    <Button text="Testing" onPress={onPressMock} disabled />
+  );
+
+  fireEvent.press(getByText("Testing"));
+  expect(onPressMock).not.toHaveBeenCalled();
 });
---
+++ testing-config/mockComponent.js
@@ -0,0 +1,18 @@
+export const mockComponent = (moduleName, propOverrideFn = () => {}) => {
+  const RealComponent = require.requireActual(moduleName);
+
+  const React = require("react");
+  const CustomizedComponent = props => {
+    return React.createElement(
+      "CustomizedComponent",
+      {
+        ...props,
+        ...propOverrideFn(props)
+      },
+      props.children
+    );
+  };
+
+  CustomizedComponent.propTypes = RealComponent.propTypes;
+  return CustomizedComponent;
+};
