4232afb47009ab1d2050d3f31e889a83c74f9d8f
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-02-22 13:14:16 -0600

    Unit Test: Targeting Elements by Text

---
+++ App/components/__tests__/Form.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { TextField } from "../Form";
+
+test("renders the passed label", () => {
+  const { getByText, queryByText } = render(<TextField label="Test Label" />);
+
+  expect(getByText("Test Label")).not.toBeNull();
+  expect(queryByText("ASDF")).toBeNull();
+});
